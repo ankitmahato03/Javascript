587858e286c4ee972dc793c26b274929ab029394
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2024-09-04 16:15:31 +0000

    javas

---
+++ basic/string.js
@@ -0,0 +1,7 @@
+let str="               hello my name is ankit             ";
+
+console.log(str.slice(0,16))
+console.log(str.toUpperCase())
+console.log(str.trim())
+console.log(str.charAt(16))
+console.log(str.indexOf("t"))
