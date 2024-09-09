f95661e28c22659e720c6b41e6e86529777b4c0a
ankitmahato03 <ankitmahato1212@gmail.com>
2024-09-09 23:53:08 +0530

    math

---
+++ basic/math.js
@@ -0,0 +1,14 @@
+const score = 400;
+console.log(score);
+
+// the score assign value javascript convert into number although it it nat a number
+
+const balance = new Number(100)
+console.log(balance);
+// this is a number  typecastd
+
+console.log(balance.toFixed(2));
+const number = 1245.00
+
+console.log(number.toPrecision(2))
+//if you use precision keep in mind that the value goes round of after the number which you passed in precision in this case which is 2
