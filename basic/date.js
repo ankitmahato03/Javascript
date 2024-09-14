0cc9d68ae9045d92f61df708aee0f4edd4faa2bf
ankitmahato03 <ankitmahato1212@gmail.com>
2024-09-14 17:07:34 +0530

    dates

--- basic/date.js
+++ basic/date.js
@@ -1,13 +1,47 @@
+const myTimestamp = Date.now();
 
-function teminalTime(){
-    setInterval(()=>{
-        console.clear();
-    const currentDate= new Date();
-    let hours=currentDate.getHours();
-    let minute= currentDate.getMinutes();
+console.log(myTimestamp);
+
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString("default", {
+  weekday: "long",
+});
+
+function teminalTime() {
+  setInterval(() => {
+    console.clear();
+    const currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minute = currentDate.getMinutes();
     let second = currentDate.getSeconds();
-    console.log(`The time is ${hours}:${minute}:${second}`)
-    },1000)
+    console.log(`The time is ${hours}:${minute}:${second}`);
+  }, 1000);
 }
 
 teminalTime();
