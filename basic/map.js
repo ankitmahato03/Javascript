ae12817af2db4c864d757b2228e60ebfb6c4d940
ankitmahato03 <ankitmahato1212@gmail.com>
2024-11-29 18:22:11 +0530

    initial push to git

---
+++ basic/map.js
@@ -0,0 +1,28 @@
+let arr = [1, 2, 3, 4, 5, 6, 5, 7, 8, 7];
+
+// let userInput = prompt("Please enter your name:");
+// let n = Number(userInput);
+// function multi(i) {
+//   return i * n;
+// }
+
+let newarr=[];
+for (let i = 0; i < arr.length; i++) {
+    
+    if (arr[i] % 2 ==0 ) {
+        newarr.push(arr[i])
+        
+    }
+    
+  
+    
+}
+
+
+
+
+
+
+
+//const ans = arr.map(multi);
+console.log(newarr);
